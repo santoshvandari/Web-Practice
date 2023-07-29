eda54a287e27ecba62451bdba668cb7c3690e32b
santoshvandari <santoshvandari1@gmail.com>
2023-07-29 18:21:02 +0545

    reWritten in Tailwind

---
+++ TailwindCSS/PersonalWebsite/script.js
@@ -0,0 +1,10 @@
+"use strict"
+document.addEventListener("DOMContentLoaded",function(){
+  // Auto typing text 
+  let typing =  new Typed("#typed",{
+      strings:["Web Developer","Wordpress Developer","Video Editor","Content Writer","Graphic Designer","Learner"],
+      typeSpeed: 120,
+      backSpeed: 60,
+      loop: true
+  })
+})

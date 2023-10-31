868db4cba456c8e4a89c9a8c67382f4ba1fe5464
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2023-10-31 10:25:26 +0545

    Initial

--- JS Practice/Tic Tac Toe Game/script.js
+++ JS Practice/Tic Tac Toe Game/script.js
@@ -37,5 +37,6 @@ setInterval(function(){
 //Adding the Event Listener in the Box
 let BoxCollectiion = document.getElementsByClassName('box');
 Array.from(BoxCollectiion).forEach(element=>{
-    
+    let boxtext=element.getElementsByClassName('boxtext')[0];
+    element.addEventListener
 })
